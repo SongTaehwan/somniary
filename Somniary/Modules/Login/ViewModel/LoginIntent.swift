a41a8abe2613291521dc4be39d9f39ab4237d7a5
송태환 <axoghks@gmail.com>
2025-09-19 22:34:02 +0900

    feat: :construction: LoginIntent 정의

---
+++ Somniary/Modules/Login/ViewModel/LoginIntent.swift
@@ -0,0 +1,58 @@
+//
+//  LoginIntent.swift
+//  Somniary
+//
+//  Created by 송태환 on 9/19/25.
+//
+
+import Foundation
+
+// MARK: Intent
+internal enum LoginIntent: Equatable {
+
+    // MARK: View Lifecycle
+    enum LifecycleIntent: Equatable { case appeared, disappeared }
+
+    // MARK: 푸시, 딥링크, 앱 설정 변경, 네트워크 재연결
+    enum ExtenralIntent: Equatable { case deepLink(URL), networkReachable(Bool) }
+
+    // MARK: Internal(Effect)
+    enum InternalIntent: Equatable {
+        case loginResponse(Result<Token, LoginError>)  // 로그인 API 결과 처리
+        case signupResponse(Result<Token, LoginError>) // 회원가입 API 결과 처리
+    }
+
+    // MARK: User interaction
+    enum UserIntent: Equatable {
+        case emailChanged(String)
+        case otpCodeChanged(String)
+        case loginTapped
+        case signUpTapped
+        case appleSignInTapped // SDK 연결
+        case googleSignInTapped // SDK 연결
+        case signupCompletionTapped
+    }
+
+    // MARK: Navigation
+    enum NavigationIntent: Equatable {
+        case routeToHome
+        case routeToSignUp
+        case routeToVerification
+        case routeToSignUpCompletion
+    }
+
+    // Lifecycle(View)
+    case lifecycle(LifecycleIntent)
+
+    // User(Gesture)
+    case user(UserIntent)
+
+    // External(System) - 푸시, 딥링크, 앱 설정 변경, 네트워크 재연결
+    case systemExtenral(ExtenralIntent)
+
+    // Internal(Effect)
+    case systemInternal(InternalIntent)
+
+    // Navigation
+    case navigation(NavigationIntent)
+}
