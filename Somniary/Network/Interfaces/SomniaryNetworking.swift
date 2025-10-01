ea46d832113baadc734f71f449c267af94ad3224
송태환 <axoghks@gmail.com>
2025-10-01 10:22:40 +0900

    feat: :sparkles: Combine 유틸리티 추가

--- Somniary/Network/Interfaces/SomniaryNetworking.swift
+++ Somniary/Network/Interfaces/SomniaryNetworking.swift
@@ -6,9 +6,26 @@
 //
 
 import Foundation
+import Combine
 
 protocol SomniaryNetworking<Target> {
     associatedtype Target
 
     func request<T: Decodable>(_ endpoint: Target, type: T.Type) async throws -> T
+}
+
+extension SomniaryNetworking {
+    func publisher<T: Decodable>(_ endpoint: Target, type: T.Type) -> AnyPublisher<T, Error> {
+        return Future { promise in
+            Task {
+                do {
+                    let result = try await request(endpoint, type: type)
+                    promise(.success(result))
+                } catch {
+                    promise(.failure(error))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
 }
