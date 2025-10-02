8454006cd99d3cfbf5641775b00ce056bc14383e
송태환 <axoghks@gmail.com>
2025-10-02 20:07:16 +0900

    refactor: :recycle: 백엔드 url 설정

--- Somniary/Network/Interfaces/SomniaryEndpoint.swift
+++ Somniary/Network/Interfaces/SomniaryEndpoint.swift
@@ -6,12 +6,25 @@
 //
 
 import Foundation
+import Alamofire
 
 protocol SomniaryEndpoint: Endpoint {}
 
+/// 도메인 공통 로직 추가
 extension SomniaryEndpoint {
-
+    /// DIP 트레이드 오프
     var baseURL: URL {
-        return URL(string: "https://api.somniary.io")!
+        guard let url = URL(string: AppInfo.shared.domainServiceURL) else {
+            fatalError("AppInfo.shared.domainServiceURL is nil")
+        }
+
+        return url
+    }
+
+    /// DIP 트레이드 오프
+    var headers: HTTPHeaders {
+        return HTTPHeaders([
+            "apiKey": AppInfo.shared.domainClientKey
+        ])
     }
 }
