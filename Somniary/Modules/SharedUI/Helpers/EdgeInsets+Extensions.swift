7fe7259a2394bd68d6580a439173fbff3cd9cefe
송태환 <axoghks@gmail.com>
2025-10-08 17:21:57 +0900

    feat: :sparkles: 유틸리티 추가

---
+++ Somniary/Modules/SharedUI/Helpers/EdgeInsets+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  EdgeInsets+Extensions.swift
+//  Somniary
+//
+//  Created by 송태환 on 10/5/25.
+//
+
+import SwiftUI
+
+extension EdgeInsets {
+    static var zero: EdgeInsets {
+        return Self.init(top: 0, leading: 0, bottom: 0, trailing: 0)
+    }
+}
