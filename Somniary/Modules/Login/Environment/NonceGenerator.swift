e0476c2be858b516068e912cbd061c32e4b4f501
송태환 <axoghks@gmail.com>
2025-10-11 16:59:33 +0900

    feat: :sparkles: 애플 로그인용 nonce 생성 유틸리티 추가

---
+++ Somniary/Modules/Login/Environment/NonceGenerator.swift
@@ -0,0 +1,54 @@
+//
+//  NonceGenerator.swift
+//  Somniary
+//
+//  Created by 송태환 on 10/11/25.
+//
+
+import Foundation
+import CryptoKit
+
+struct NonceGenerator: CryptoProviding {
+
+    static let shared = NonceGenerator()
+
+    private init() {}
+
+    func generateSecureRandom(length: Int = 32) -> String {
+        precondition(length > 0)
+
+        let charset: [Character] = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0..<16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+
+            randoms.forEach { random in
+                if remainingLength == 0 { return }
+
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+
+        return result
+    }
+
+    func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        return hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+    }
+}
