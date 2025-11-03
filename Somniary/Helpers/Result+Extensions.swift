1ff28b6a3268d884826a7ada9b4c9aa619bb4d85
송태환 <axoghks@gmail.com>
2025-11-04 00:14:24 +0900

    feat: :sparkles: Result 타입 유틸리티 추가

--- Somniary/Helpers/Result+Extensions.swift
+++ Somniary/Helpers/Result+Extensions.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 extension Result where Failure == Error {
+    // async with error mapper
     static func catching(_ body: () async throws -> Success) async -> Self {
         do {
             return .success(try await body())
@@ -15,12 +16,31 @@ extension Result where Failure == Error {
             return .failure(error)
         }
     }
+
+    // sync with error mapper
+    static func catching(_ body: () throws -> Success) -> Self {
+        do {
+            return .success(try body())
+        } catch {
+            return .failure(error)
+        }
+    }
 }
 
 extension Result {
+    // async task with error mapper
     static func catching(_ body: () async throws -> Success, mapError: (Error) -> Failure) async -> Self {
         do {
             return .success(try await body())
+        } catch {
+            return .failure(mapError(error))
+        }
+    }
+
+    // sync with error mapper
+    static func catching(_ body: () throws -> Success, mapError: (Error) -> Failure) -> Self {
+        do {
+            return .success(try body())
         } catch {
             return .failure(mapError(error))
         }
